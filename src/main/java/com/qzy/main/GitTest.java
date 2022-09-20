0112438226f789f500c39c18d88d8e72fb65a7a1
qzy1027 <okita1027@outlook.com>
2022-09-20 08:37:18 +0800

    my second commit -> GitTest

---
+++ src/main/java/com/qzy/main/GitTest.java
@@ -0,0 +1,13 @@
+package com.qzy.main;
+
+/**
+ * @Author qzy
+ * @ClassName GitTest
+ * @Description TODO(一句话描述该类功能)
+ * @Date 2022/9/20 8:35 星期二
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
