784f87aa93ad40cf87869fe7ee745834b5c51d34
qzy1027 <okita1027@outlook.com>
2022-09-20 08:47:47 +0800

    my third commit -> GitTest

--- src/main/java/com/qzy/main/GitTest.java
+++ src/main/java/com/qzy/main/GitTest.java
@@ -9,5 +9,6 @@ package com.qzy.main;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("HelloGit - Version - 3");
     }
 }
